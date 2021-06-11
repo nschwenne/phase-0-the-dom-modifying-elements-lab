ee81d1a08b4f694d49c572200d0a3ec095040336
Nicholas <nschwenne@gmail.com>
2021-06-11 10:44:32 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1>'Nick is the champion'</h1>";
